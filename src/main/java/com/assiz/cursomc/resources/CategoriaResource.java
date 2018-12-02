4fa901bfdceedb23fb657e7bd35d53cde8a1fa90
Rogerio Assiz <rogerio.assiz@uol.com.br>
2018-12-02 00:00:00 -0200

    Produto e associação muitos para muitos

--- src/main/java/com/assiz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/assiz/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.assiz.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.assiz.cursomc.domain.Categoria;
+import com.assiz.cursomc.domains.Categoria;
 import com.assiz.cursomc.services.CategoriaService;
 
 @RestController
--- src/main/java/com/assiz/cursomc/services/CategoriaService.java
+++ src/main/java/com/assiz/cursomc/services/CategoriaService.java
@@ -3,7 +3,7 @@ package com.assiz.cursomc.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.assiz.cursomc.domain.Categoria;
+import com.assiz.cursomc.domains.Categoria;
 import com.assiz.cursomc.repositories.CategoriaRepository;
 
 @Service
